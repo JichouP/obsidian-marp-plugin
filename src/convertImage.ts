fab93b8007d2050ff3ba8fb97185c2f0ce58de1a
JichouP <akkunn100@gmail.com>
2023-02-28 22:09:02 +0900

    ✨ use resourceUrl to preview and base64 to export

---
+++ src/convertImage.ts
@@ -0,0 +1,87 @@
+import { access, readFile } from 'fs/promises';
+import { FileSystemAdapter, normalizePath } from 'obsidian';
+import mimes from 'mime/lite';
+
+const prefix = 'app://local';
+
+async function readFileAsBase64(path: string): Promise<string | null> {
+  const mime = mimes.getType(path);
+  if (!mime) return null;
+
+  try {
+    return `data:${mime};base64,${await readFile(path, {
+      encoding: 'base64',
+    })}`;
+  } catch (e) {
+    return null;
+  }
+}
+
+async function convertPathToLocalLink(path: string): Promise<string | null> {
+  if (await app.vault.adapter.exists(path)) {
+    return app.vault.adapter.getResourcePath(path);
+  }
+
+  try {
+    await access(path);
+    return `${prefix}/${normalizePath(path)}`;
+  } catch (e) {
+    return null;
+  }
+}
+
+async function convertToBase64(path: string): Promise<string | null> {
+  if (await app.vault.adapter.exists(path)) {
+    const basePath = (
+      this.app.vault.adapter as FileSystemAdapter
+    ).getBasePath();
+    return readFileAsBase64(normalizePath(`${basePath}/${path}`));
+  }
+
+  try {
+    await access(path);
+    return readFileAsBase64(normalizePath(path));
+  } catch (e) {
+    try {
+      if (path.startsWith(prefix)) {
+        // remove `app://local`
+        const newPath = path.slice(prefix.length);
+        await access(newPath);
+        return readFileAsBase64(normalizePath(newPath));
+      }
+    } catch (e) {
+      return null;
+    }
+    return null;
+  }
+}
+
+export async function embedImageToHtml(html: string): Promise<Document> {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html, 'text/html');
+  const images = doc.getElementsByTagName('img');
+  for (let i = 0; i < images.length; i++) {
+    const el = images[i];
+    const src = el.getAttribute('src');
+    if (!src) continue;
+    const link = await convertToBase64(decodeURI(src));
+    if (!link) continue;
+    el.setAttribute('src', link);
+  }
+  return doc;
+}
+
+export async function convertHtml(html: string): Promise<Document> {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html, 'text/html');
+  const images = doc.getElementsByTagName('img');
+  for (let i = 0; i < images.length; i++) {
+    const el = images[i];
+    const src = el.getAttribute('src');
+    if (!src) continue;
+    const link = await convertPathToLocalLink(decodeURI(src));
+    if (!link) continue;
+    el.setAttribute('src', link);
+  }
+  return doc;
+}
--- src/export.ts
+++ src/export.ts
@@ -1,5 +1,13 @@
 import { exec } from 'child_process';
+import { readFile, rm, writeFile } from 'fs/promises';
 import { normalizePath, Notice, TFile } from 'obsidian';
+import MarkdownIt from 'markdown-it';
+import { embedImageToHtml } from './convertImage';
+import markdownItFrontMatter from 'markdown-it-front-matter';
+import { unified } from 'unified';
+import rehypeParse from 'rehype-parse/lib';
+import rehypeRemark from 'rehype-remark';
+import remarkStringify from 'remark-stringify';
 
 export async function exportSlide(
   file: TFile,
@@ -10,10 +18,34 @@ export async function exportSlide(
   const exportDir = normalizePath(`${process.env.USERPROFILE}/Downloads`);
   if (!file) return;
   const filePath = normalizePath(`${basePath}/${file.path}`);
-  const cmd = `npx -y @marp-team/marp-cli@latest --stdin false --theme-set "${themeDir}" -o "${exportDir}/${file.basename}.${ext}" -- "${filePath}"`;
+  const tmpPath = `${exportDir}/${file.basename}.tmp`;
+
+  let frontMatter;
+
+  const md = new MarkdownIt().use(markdownItFrontMatter, v => {
+    frontMatter = v;
+  });
+
+  const result = md.render(await readFile(filePath, 'utf-8'));
+  const embedded = await embedImageToHtml(result);
+
+  let reverseConverted = (
+    await unified()
+      .use(rehypeParse)
+      .use(rehypeRemark)
+      .use(remarkStringify)
+      .process(embedded.body.innerHTML)
+  ).value;
+
+  reverseConverted = `---\n${frontMatter}\n---\n${reverseConverted}`;
+
+  await writeFile(tmpPath, reverseConverted);
+
+  const cmd = `npx -y @marp-team/marp-cli@latest --stdin false --allow-local-files --theme-set "${themeDir}" -o "${exportDir}/${file.basename}.${ext}" -- "${tmpPath}"`;
 
   new Notice(`Exporting "${file.basename}.${ext}" to "${exportDir}"`, 20000);
   exec(cmd, () => {
     new Notice('Exported successfully', 20000);
+    rm(tmpPath);
   });
 }
