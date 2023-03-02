eebd5a6aa9d04d991ae39b01667ec72da4dca9e1
Chivier Humber <chivier.humber@outlook.com>
2023-03-02 19:04:32 +0800

    fix: MaxOS command not found issue

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -30,6 +30,8 @@ const context = await esbuild.context({
     '@lezer/common',
     '@lezer/highlight',
     '@lezer/lr',
+    'node:os',
+    'node:process',
     ...builtins,
   ],
   format: 'cjs',
