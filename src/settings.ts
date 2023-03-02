9f7ee4f04f39ac14f8c030c1d7d7edf6d5335272
JichouP <akkunn100@gmail.com>
2023-03-02 23:15:42 +0900

    ✨ Added the option to choose whether to use the fragmented list or not

--- src/settings.ts
+++ src/settings.ts
@@ -2,10 +2,12 @@ export interface MarpPluginSettings {
   autoReload: boolean;
   createNewSplitTab: boolean;
   themeDir: string;
+  fragmentedList: boolean;
 }
 
 export const MARP_DEFAULT_SETTINGS: MarpPluginSettings = {
   autoReload: true,
   createNewSplitTab: true,
   themeDir: 'MarpTheme',
+  fragmentedList: false,
 };
