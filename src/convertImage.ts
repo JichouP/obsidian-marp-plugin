28e66c5762c31046a9d4788e42921d6bf3e3c5c8
Akira Kobayashi <31063176+JichouP@users.noreply.github.com>
2023-03-03 22:56:17 +0900

    ✨ use regex to embed image and embed image from web (#32)
    * 🚧 test for match image by regexp
    
    * 🚧 wip
    
    * ✨ use regex to embed image and embed image from web

--- src/convertImage.ts
+++ src/convertImage.ts
@@ -1,20 +1,19 @@
 import { access, readFile } from 'fs/promises';
-import { FileSystemAdapter } from 'obsidian';
+import { FileSystemAdapter, requestUrl } from 'obsidian';
 import { join, normalize } from 'path';
-
 import mimes from 'mime/lite';
 
 const prefix = 'app://local';
 
 async function readFileAsBase64(path: string): Promise<string | null> {
-  const mime = mimes.getType(path);
-  if (!mime) return null;
-
   try {
-    return `data:${mime};base64,${await readFile(path, {
-      encoding: 'base64',
-    })}`;
-  } catch (e) {
+    return addMimeToBase64Data(
+      path,
+      await readFile(path, {
+        encoding: 'base64',
+      }),
+    );
+  } catch {
     return null;
   }
 }
@@ -27,12 +26,14 @@ async function convertPathToLocalLink(path: string): Promise<string | null> {
   try {
     await access(path);
     return `${prefix}/${normalize(path)}`;
-  } catch (e) {
+  } catch {
     return null;
   }
 }
 
-async function convertToBase64(path: string): Promise<string | null> {
+export async function convertToBase64(path: string): Promise<string | null> {
+  const mime = mimes.getType(path);
+  if (!mime) return null;
   if (await app.vault.adapter.exists(path)) {
     const basePath = (
       this.app.vault.adapter as FileSystemAdapter
@@ -43,51 +44,40 @@ async function convertToBase64(path: string): Promise<string | null> {
   try {
     await access(path);
     return readFileAsBase64(normalize(path));
-  } catch (e) {
-    try {
-      if (path.startsWith(prefix)) {
-        // remove `app://local`
-        const newPath = path.slice(prefix.length);
-        await access(newPath);
-        return readFileAsBase64(normalize(newPath));
-      }
-    } catch (e) {
-      return null;
+  } catch {
+    /* empty */
+  }
+
+  try {
+    if (path.startsWith(prefix)) {
+      // remove `app://local`
+      const newPath = path.slice(prefix.length);
+      await access(newPath);
+      return readFileAsBase64(normalize(newPath));
     }
+  } catch {
+    /* empty */
+  }
+
+  // try to get image from web
+  return urlToBase64(path);
+}
+
+async function urlToBase64(url: string): Promise<string | null> {
+  try {
+    return addMimeToBase64Data(
+      url,
+      Buffer.from(await requestUrl(url).arrayBuffer).toString('base64'),
+    );
+  } catch {
     return null;
   }
 }
 
-export async function embedImageToHtml(html: string): Promise<Document> {
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(html, 'text/html');
-  const images = doc.getElementsByTagName('img');
-  for (let i = 0; i < images.length; i++) {
-    const el = images[i];
-    const src = el.getAttribute('src');
-    if (!src) continue;
-    const link = await convertToBase64(decodeURI(src));
-    if (!link) continue;
-    el.setAttribute('src', link);
-  }
-  const figures = doc.getElementsByTagName('figure');
-  const reg = /background-image:url\("([^)]+)"\)/;
-  for (let i = 0; i < figures.length; i++) {
-    const el = figures[i];
-    const style = el.getAttribute('style');
-    if (!style || !style.contains('background-image:url')) continue;
-    const result = style.match(reg);
-    const matched = result?.at(1);
-    if (!matched) continue;
-    const converted = await convertToBase64(decodeURI(matched));
-    if (!converted) continue;
-    const replaced = result?.input
-      ?.replace(matched, converted)
-      .replace(/\\/g, '/');
-    if (!replaced) continue;
-    el.setAttribute('style', replaced);
-  }
-  return doc;
+function addMimeToBase64Data(path: string, data: string): string | null {
+  const mime = mimes.getType(path);
+  if (!mime) return null;
+  return `data:${mime};base64,${data}`;
 }
 
 export async function convertHtml(html: string): Promise<Document> {
--- src/export.ts
+++ src/export.ts
@@ -1,22 +1,17 @@
 import { exec } from 'child_process';
 import { access, mkdir, readFile, rm, writeFile } from 'fs/promises';
 import { Notice, TFile } from 'obsidian';
-import MarkdownIt from 'markdown-it';
-import { embedImageToHtml } from './convertImage';
-import markdownItFrontMatter from 'markdown-it-front-matter';
-import { unified } from 'unified';
-import rehypeParse from 'rehype-parse/lib';
-import rehypeRemark from 'rehype-remark';
-import remarkStringify from 'remark-stringify';
+import { convertToBase64 } from './convertImage';
 import { join, normalize } from 'path';
 import fixPath from 'fix-path';
+
+const imgPathReg = /!\[[^\]]*\]\(([^)]+)\)/g;
 
 export async function exportSlide(
   file: TFile,
   ext: 'html' | 'pdf' | 'pptx',
   basePath: string,
   themeDir: string,
-  fragmentedList: boolean,
 ) {
   const exportDir = join(
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
@@ -27,31 +22,27 @@ export async function exportSlide(
   const filePath = normalize(join(basePath, file.path));
   const tmpPath = join(exportDir, `${file.basename}.tmp`);
 
-  let frontMatter;
+  let fileContent = await readFile(filePath, 'utf-8');
 
-  const md = new MarkdownIt('commonmark', { breaks: true }).use(
-    markdownItFrontMatter,
-    v => {
-      frontMatter = v;
-    },
+  const srcBase64TupleList = await Promise.all(
+    [...new Set([...fileContent.matchAll(imgPathReg)].map(v => v[1]))].map(
+      async v => [v, await convertToBase64(v)] as const,
+    ),
   );
 
-  const result = md.render(await readFile(filePath, 'utf-8'));
-  const embedded = await embedImageToHtml(decode(result));
-
-  let reverseConverted = (
-    await unified()
-      .use(rehypeParse)
-      .use(rehypeRemark)
-      .use(remarkStringify, { bullet: fragmentedList ? '*' : '-' })
-      .process(embedded.body.innerHTML)
-  ).value;
-
-  reverseConverted = `---\n${frontMatter}\n---\n${reverseConverted}`;
+  for (const [src, base64] of srcBase64TupleList) {
+    fileContent = fileContent.replace(
+      new RegExp(
+        String.raw`(!\[[^\]]*\])\(${src.replace(/\\/g, '\\\\')}\)`,
+        'g',
+      ),
+      `$1(${base64})`,
+    );
+  }
 
   await mkdir(exportDir, { recursive: true });
   try {
-    await writeFile(tmpPath, reverseConverted.replace(/\\_/g, '_'));
+    await writeFile(tmpPath, fileContent);
   } catch (e) {
     console.error(e);
   }
@@ -76,12 +67,4 @@ export async function exportSlide(
     new Notice('Exported successfully', 20000);
     rm(tmpPath);
   });
-}
-
-function decode(str: string) {
-  const txt = document.createElement('textarea');
-
-  txt.innerHTML = str;
-
-  return txt.value;
 }
